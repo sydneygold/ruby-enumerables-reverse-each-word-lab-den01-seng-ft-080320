26426400c61e8089ac11d01b0a39afa8df24bd66
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-22 21:20:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,7 @@
-#def reverse_each_word(array, element)
-#  array = string.split(' ')
-#array.each do |element|
-#     puts element.reverse!
-#return array
-#  end
-#end
-
 def reverse_each_word(string)
   array = []
   string.split(' ').collect do |element|
       array << element.reverse!
-
-end
+  end
 return array.join(" ")
 end
